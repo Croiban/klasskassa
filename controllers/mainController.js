a06bbe017d1ddca5ca78ff5acb8c2acf2d65bc7e
Robert Madeley <robert.madeley@cambio.se>
2023-11-15 22:35:56 +0100

    some more front end

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,7 @@
 const catchAsync = require('../utils/catchAsync');
 
 const mainController = catchAsync(async (req, res, next) => {
-  res.status(200).render('mainView');
+  res.status(200).render('mainView', { balance: 1500, goal: 35000 });
 });
 
 module.exports = mainController;
