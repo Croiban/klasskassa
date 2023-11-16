67b959e26a3ea8d53e3ac5bba23975b0224f73c1
Robert Madeley <robert.madeley@cambio.se>
2023-11-16 19:47:30 +0100

    no animations :(

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,10 @@
 const catchAsync = require('../utils/catchAsync');
 
 const mainController = catchAsync(async (req, res, next) => {
-  res.status(200).render('mainView', { balance: 1500, goal: 35000 });
+  res.status(200).render('mainView', {
+    balance: process.env.balance || 1234,
+    goal: process.env.goal || 45678,
+  });
 });
 
 module.exports = mainController;
